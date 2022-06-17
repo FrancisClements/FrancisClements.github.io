79f3e096e1d01a3ced2968cf78f55c0a8c4d9334
Francis Clemente <francisclemente727@yahoo.com.ph>
2022-06-17 23:37:12 +0800

    add deploy sh

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+#taken from vite docs
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:FrancisClements/FrancisClements.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+
+cd -
